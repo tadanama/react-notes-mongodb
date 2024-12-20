07e0e362bb21b0daf3ae9072629687a0bfe8ab5f
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-20 16:08:22 +0800

    Include Header in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<>
-			<div className="container"></div>
+			<div className="container">
+				<Header />
+			</div>
 		</>
 	);
 }
