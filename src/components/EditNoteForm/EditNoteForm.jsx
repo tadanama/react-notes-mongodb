c2283f7ada606cf245eaba910e407f85e99104db
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-20 17:21:21 +0800

    Change form-container class name to edit-form-container
    Differentiate styles applied to EditNoteForm and AddNoteForm
    
    Add icon to save the edited note that act as a button and style it

--- src/components/EditNoteForm/EditNoteForm.jsx
+++ src/components/EditNoteForm/EditNoteForm.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./EditNoteForm.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 function EditNoteForm() {
-  return (
+	return (
 		<>
-			<div className="form-container">
+			<div className="edit-form-container">
 				<h1>Edit a note</h1>
 				<form>
 					<input type="text" name="title" placeholder="Title" />
 					<br />
 					<textarea placeholder="Content" />
+					<button className="save-icon">
+						<FontAwesomeIcon icon={faFloppyDisk} />
+					</button>
 				</form>
 			</div>
 		</>
 	);
 }
 
-export default EditNoteForm
+export default EditNoteForm;
