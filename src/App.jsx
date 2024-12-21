b54a42ff4223724e649c9960cf2b3fed86d00e8f
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-21 23:38:18 +0800

    Configure routes to render different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import AddNoteForm from "./components/AddNoteForm/AddNoteForm";
+import EditNoteForm from "./components/EditNoteForm/EditNoteForm";
+import NotesList from "./components/NotesList/NotesList";
 
 function App() {
 	return (
 		<>
-			<div className="container">
-				<Header />
-			</div>
+			<BrowserRouter>
+				<div className="container">
+					<Header />
+					<Routes>
+						<Route path="/" element={<NotesList />} />
+
+						<Route path={"/note"}>
+							<Route index element={<AddNoteForm />} />
+							<Route path=":id" element={<EditNoteForm />} />
+						</Route>
+
+						<Route path="*" element={<h1>Page not found</h1>}/>
+					</Routes>
+					{/* <Routes>
+						<Route path="/" element={<NotesList/>}/>
+						<Route path="/add" element={<AddNoteForm/>}/>
+						<Route path="/edit/:id" element={<EditNoteForm/>}/>
+					</Routes> */}
+				</div>
+			</BrowserRouter>
 		</>
 	);
 }
