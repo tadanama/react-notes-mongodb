ff5a819551af0a1449a97ecdb141ebe9630ae1aa
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-21 12:22:19 +0800

    Track AddNoteForm input and textarea with useState

--- src/components/AddNoteForm/AddNoteForm.jsx
+++ src/components/AddNoteForm/AddNoteForm.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddNoteForm.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AddNoteForm() {
+	// Track title and content input state
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+
 	return (
 		<>
 			<div className="add-form-container">
 				<h1>Create a note</h1>
 				<form>
-					<input type="text" name="title" placeholder="Title" />
+					<input
+						type="text"
+						name="title"
+						placeholder="Title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
 					<br />
-					<textarea placeholder="Content" />
+					<textarea
+						placeholder="Content"
+						value={content}
+						onChange={(e) => setContent(e.target.value)}
+					/>
 					<button className="add-icon">
 						<FontAwesomeIcon icon={faPlus} />
 					</button>
