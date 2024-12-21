a8a7c334fc6df366cd3ecbeedd4747cb70541b51
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-21 12:02:21 +0800

    Create NotesList component structure

---
+++ src/components/NotesList/NotesList.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+import "./NotesList.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+function NotesList() {
+	return (
+		<div className="notes-container">
+			<h2>My notes</h2>
+			<div className="notes-grid-container">
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+				<div className="note-card">
+					<h3>Note title</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						dolorum et asperiores quae repudiandae enim voluptatibus saepe id
+						cumque amet.
+					</p>
+					<div className="bottom">
+						<span>20/12/2024</span>
+						<button className="delete-icon">
+							<FontAwesomeIcon icon={faTrash} />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default NotesList;
