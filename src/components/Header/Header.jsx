5352c565581dd8a953ec02ff728db9d44fdceb15
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-21 12:17:36 +0800

    Track search input with useState

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,19 @@ import "./Header.css";
 function Header() {
 	// State to toggle display nav links
 	const [isOpen, setIsOpen] = useState(false);
+	// Search input state
+	const [search, setSearch] = useState("");
 
 	return (
 		<div className="header-container">
 			<h1>Notes</h1>
-			<input type="text" className="search-input" placeholder="Search" />
+			<input
+				type="text"
+				className="search-input"
+				placeholder="Search"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
 			<div className="nav-links">
 				<ul className={isOpen ? "active" : null}>
 					<li>Create note</li>
