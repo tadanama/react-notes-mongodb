c636d7c39b1b7fb1f4e43f390057892f91b35090
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-21 12:26:39 +0800

    Track EditNoteForm input and textarea state with useState

--- src/components/EditNoteForm/EditNoteForm.jsx
+++ src/components/EditNoteForm/EditNoteForm.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EditNoteForm.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 function EditNoteForm() {
+	// Track title and content input state
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+
 	return (
 		<>
 			<div className="edit-form-container">
 				<h1>Edit a note</h1>
 				<form>
-					<input type="text" name="title" placeholder="Title" />
+					<input
+						type="text"
+						name="title"
+						placeholder="Title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
 					<br />
-					<textarea placeholder="Content" />
+					<textarea
+						placeholder="Content"
+						value={content}
+						onChange={(e) => setContent(e.target.value)}
+					/>
 					<button className="save-icon">
 						<FontAwesomeIcon icon={faFloppyDisk} />
 					</button>
