6c352027b64fb11a841f62a23eda8110d9a0e0cb
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-22 20:52:47 +0800

    Dispatch async thunk to create new note

--- src/components/AddNoteForm/AddNoteForm.jsx
+++ src/components/AddNoteForm/AddNoteForm.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addNote } from "../../features/notes/notesSlice";
 import "./AddNoteForm.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -8,11 +11,53 @@ function AddNoteForm() {
 	const [title, setTitle] = useState("");
 	const [content, setContent] = useState("");
 
+	// State for error message
+	const [errorMessage, setErrorMessage] = useState("");
+
+	// Clear the error if user is filling in title or content input
+	useEffect(() => {
+		setErrorMessage("");
+	}, [title, content]);
+
+	// Boolean value to check if both fields are filled in
+	const canCreate = Boolean(title) && Boolean(content);
+
+	// Instantiate hook
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	function handleAddNote() {
+		if (canCreate) {
+			try {
+				// Dispatch async thunk to add a note
+				dispatch(addNote({ title, text: content })).unwrap();
+
+				// Clear the form
+				setTitle("");
+				setContent("");
+
+				// Redirect user to the homepage (NotesList)
+				navigate("/");
+			} catch (error) {
+				console.log(error);
+				console.log(first);
+			}
+		} else {
+			setErrorMessage("Both fields are required");
+		}
+	}
+
 	return (
 		<>
 			<div className="add-form-container">
 				<h1>Create a note</h1>
-				<form>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+
+						handleAddNote();
+					}}
+				>
 					<input
 						type="text"
 						name="title"
@@ -30,6 +75,7 @@ function AddNoteForm() {
 						<FontAwesomeIcon icon={faPlus} />
 					</button>
 				</form>
+				{errorMessage && <p>{errorMessage}</p>}
 			</div>
 		</>
 	);
