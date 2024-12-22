03cc4038365b89085b2f36c6d0a9c335cfe56d4c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-22 11:54:25 +0800

    Provide access to the redux store for all components
    Fetch the notes before app is rendered

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+import { getAllNotes } from "./features/notes/notesSlice.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+store.dispatch(getAllNotes());
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
+);
