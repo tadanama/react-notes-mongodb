680a0c937a47e63eb759349faf653bd4bc03dfa6
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-23 15:50:58 +0800

    Create a context to share search state with multiple component
    - Header. Let the useState hook to manage the search input state
    - NotesList. Display note card with matching title as the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext, useState } from "react";
 import Header from "./components/Header/Header";
 import AddNoteForm from "./components/AddNoteForm/AddNoteForm";
 import EditNoteForm from "./components/EditNoteForm/EditNoteForm";
 import NotesList from "./components/NotesList/NotesList";
 
+export const SearchContext = createContext();
+
 function App() {
+	const [search, setSearch] = useState("");
+
 	return (
 		<>
-			<BrowserRouter>
-				<div className="container">
-					<Header />
-					<Routes>
-						<Route path="/" element={<NotesList />} />
+			<SearchContext.Provider value={{ search, setSearch }}>
+				<BrowserRouter>
+					<div className="container">
+						<Header />
+						<Routes>
+							<Route path="/" element={<NotesList />} />
 
-						<Route path={"/note"}>
-							<Route index element={<AddNoteForm />} />
-							<Route path=":noteId" element={<EditNoteForm />} />
-						</Route>
+							<Route path={"/note"}>
+								<Route index element={<AddNoteForm />} />
+								<Route path=":noteId" element={<EditNoteForm />} />
+							</Route>
 
-						<Route path="*" element={<h1>Page not found</h1>}/>
-					</Routes>
-					{/* <Routes>
+							<Route path="*" element={<h1>Page not found</h1>} />
+						</Routes>
+						{/* <Routes>
 						<Route path="/" element={<NotesList/>}/>
 						<Route path="/add" element={<AddNoteForm/>}/>
 						<Route path="/edit/:id" element={<EditNoteForm/>}/>
 					</Routes> */}
-				</div>
-			</BrowserRouter>
+					</div>
+				</BrowserRouter>
+			</SearchContext.Provider>
 		</>
 	);
 }
