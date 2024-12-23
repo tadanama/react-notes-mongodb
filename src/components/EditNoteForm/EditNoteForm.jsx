13a91988fe6815731cccaf2a68f7daf5359d9597
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-23 08:07:33 +0800

    Dispatch the updateNote async thunk when user clicks add button
    - Get all data of the note to update from redux store
    - Set value of title and content to the data from state
    - Redirect user back to homepage (/) after successful updating

--- src/components/EditNoteForm/EditNoteForm.jsx
+++ src/components/EditNoteForm/EditNoteForm.jsx
@@ -1,18 +1,65 @@
 import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllNotes, updateNote } from "../../features/notes/notesSlice";
 import "./EditNoteForm.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 
 function EditNoteForm() {
+	// Get the id of the current note from the path parameter
+	const { noteId } = useParams();
+	// console.log(useParams());
+
+	// Instantiate hook
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	// Get the note with the id from path parameter
+	const notes = useSelector(selectAllNotes);
+	const currentNote = notes.find((note) => note._id === noteId);
+
 	// Track title and content input state
-	const [title, setTitle] = useState("");
-	const [content, setContent] = useState("");
+	const [title, setTitle] = useState(currentNote?.title);
+	const [content, setContent] = useState(currentNote?.text);
+
+	// Check if both input is filled in
+	const canEdit = Boolean(title) && Boolean(content);
+
+	function handleUpdateNote() {
+		if (canEdit) {
+			try {
+				const updatedNote = {
+					title,
+					text: content,
+					id: noteId,
+				};
+				// Dispatch async to update note to database
+				dispatch(updateNote(updatedNote)).unwrap();
+
+				// Clear input
+				setTitle("");
+				setContent("");
+
+				// Redirect to homepage
+				navigate("/");
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	}
 
 	return (
 		<>
 			<div className="edit-form-container">
 				<h1>Edit a note</h1>
-				<form>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+
+						handleUpdateNote();
+					}}
+				>
 					<input
 						type="text"
 						name="title"
